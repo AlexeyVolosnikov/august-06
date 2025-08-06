8b554725be3e932554e4db66f0dab80e3f5348ff
Алексей Волосников <av@carbox.tech>
2025-08-06 13:17:51 +0500

    github

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
-// next.config.mjs
+// // gitlab
+// // next.config.mjs
+// export default {
+//   output: 'export',
+//   basePath: '/august-06',
+//   trailingSlash: true,
+// };
+
+// github
 export default {
   output: 'export',
+  distDir: 'docs',
   basePath: '/august-06',
   trailingSlash: true,
-};
+}
